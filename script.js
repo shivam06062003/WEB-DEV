dcd03283b2522557791f8d46376103f7b12b3623
Shivam Bhola <60916879+shivam06062003@users.noreply.github.com>
2024-05-18 11:24:15 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+// Calculator Program
+
+const display = document.getElementById("display")
+
+function appendToDisplay(input){
+    display.value += input;
+}
+function clear(){
+    display.value = "";
+}
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "error";
+    }
+}
